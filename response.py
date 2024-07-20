c3c96e51e7682277c1cf6b1ef7bdcabcd3ecd420
SupriyoOG <118549660+SupriyoOG@users.noreply.github.com>
2024-07-20 18:08:37 +0530

    Update response.py

--- response.py
+++ response.py
@@ -8,7 +8,7 @@ API_KEY = os.getenv('GEMINI_API_KEY')
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
-instruction = "You are an AI trained by the students of KPS Kadma school. Give short responses Respond in chat mode without stating your name, Nexus, in each response. When you are told to 'switch to command mode' you should say omly and only 'Switching to command mode'. And the Principle of KPS kadma is Mrs Sharmila mukherjee if you were asked 'who is the principle of kps kadma' only say the principles name when asked!"
+instruction = ""
 
 def get_response(question):
     """Get response from the AI model."""
